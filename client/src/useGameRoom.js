53d26f8e5b8e164f347c859424a0745efa7298de
thibsc <thibaut.bsc@gmail.com>
2021-03-18 21:02:51 -0400

    tab indent size

--- client/src/useGameRoom.js
+++ client/src/useGameRoom.js
@@ -10,72 +10,72 @@ const WINNER_EVENT = 'winnerEvent';
 const SOCKET_SERVER_URL = 'http://192.168.0.10:4000';
 
 const useGameRoom = (roomId, pseudo) => {
-  const [gameLogs, setGameLogs] = useState(["🙋‍♂️ welcome"]);
-  const [players, setPlayers] = useState([]);
-  const [playedCards, setPlayedCards] = useState([]);
-  const [playerCards, setPlayerCards] = useState([]);
-  const [isMaster, setMaster] = useState(false);
-  const [isPlaying, setPlaying] = useState(false);
-  const [winner, setWinner] = useState(null);
-  const socketRef = useRef();
+	const [gameLogs, setGameLogs] = useState(["🙋‍♂️ welcome"]);
+	const [players, setPlayers] = useState([]);
+	const [playedCards, setPlayedCards] = useState([]);
+	const [playerCards, setPlayerCards] = useState([]);
+	const [isMaster, setMaster] = useState(false);
+	const [isPlaying, setPlaying] = useState(false);
+	const [winner, setWinner] = useState(null);
+	const socketRef = useRef();
 
-  useEffect(() => {    
-    // Creates a WebSocket connection
-    socketRef.current = socketIOClient(SOCKET_SERVER_URL, {
-      query: { roomId, pseudo },
-    });
+	useEffect(() => {
+		// Creates a WebSocket connection
+		socketRef.current = socketIOClient(SOCKET_SERVER_URL, {
+			query: { roomId, pseudo },
+		});
 
-    // Listens for players connect/disconnect
-    socketRef.current.on(PLAYER_EVENT, (players) => {
-      console.log("new PlayerList", players);
-      setPlayers(players);
-      let selfPlayer = players.find(p => p.id === socketRef.current.id);
-      setMaster(selfPlayer.isMaster);
-      setPlaying(selfPlayer.isPlaying);
-    });
+		// Listens for players connect/disconnect
+		socketRef.current.on(PLAYER_EVENT, (players) => {
+			console.log("new PlayerList", players);
+			setPlayers(players);
+			let selfPlayer = players.find(p => p.id === socketRef.current.id);
+			setMaster(selfPlayer.isMaster);
+			setPlaying(selfPlayer.isPlaying);
+		});
 
-    // Listens for global cards update
-    socketRef.current.on(PLAYED_CARD_EVENT, (cards) => {
-      console.log("new played CardList", cards);
-      setPlayedCards(cards);
-    });
+		// Listens for global cards update
+		socketRef.current.on(PLAYED_CARD_EVENT, (cards) => {
+			console.log("new played CardList", cards);
+			setPlayedCards(cards);
+		});
 
-    // Listens for player cards update
-    socketRef.current.on(PLAYER_CARD_EVENT, (cards) => {
-      console.log("new player CardList", cards);
-      setPlayerCards(cards);
-    });
+		// Listens for player cards update
+		socketRef.current.on(PLAYER_CARD_EVENT, (cards) => {
+			console.log("new player CardList", cards);
+			setPlayerCards(cards);
+		});
 
-    // Listens for winner event
-    socketRef.current.on(WINNER_EVENT, (player) => {
-      console.log("new Winner", player);
-      setWinner(player);
-      if (!player){
-        setGameLogs(["🆕 new game"]);
-      }
-    });
+		// Listens for winner event
+		socketRef.current.on(WINNER_EVENT, (player) => {
+			console.log("new Winner", player);
+			setWinner(player);
+			if (!player) {
+				setGameLogs(["🆕 new game"]);
+			}
+		});
 
-    // Listens for current game log
-    socketRef.current.on(NEW_MASSAGE_EVENT, (message) => {
-      setGameLogs((gameLogs) => [message, ...gameLogs]);
-    });
-    
-    // Destroys the socket reference
-    // when the connection is closed
-    return () => {
-      socketRef.current.disconnect();
-    };
-  }, [roomId, pseudo]);
+		// Listens for current game log
+		socketRef.current.on(NEW_MASSAGE_EVENT, (message) => {
+			setGameLogs((gameLogs) => [message, ...gameLogs]);
+		});
 
-  // Send a message to the server
-  const sendMessage = (messageAction) => {
-    socketRef.current.emit(NEW_MASSAGE_EVENT, {
-      action: messageAction,
-      senderId: socketRef.current.id,
-    });
-  };
+		// Destroys the socket reference
+		// when the connection is closed
+		return () => {
+			socketRef.current.disconnect();
+		};
+	}, [roomId, pseudo]);
 
-  return { sendMessage, gameLogs, players, playedCards, playerCards, isMaster, isPlaying, winner };
+	// Send a message to the server
+	const sendMessage = (messageAction) => {
+		socketRef.current.emit(NEW_MASSAGE_EVENT, {
+			action: messageAction,
+			senderId: socketRef.current.id,
+		});
+	};
+
+	return { sendMessage, gameLogs, players, playedCards, playerCards, isMaster, isPlaying, winner };
 };
 
 export default useGameRoom;
--- server/timelineGameServer.js
+++ server/timelineGameServer.js
@@ -2,9 +2,9 @@ const cards = require("./Cards");
 
 const server = require("http").createServer();
 const io = require("socket.io")(server, {
-  cors: {
-    origin: "*",
-  },
+	cors: {
+		origin: "*",
+	},
 });
 
 const PORT = 4000;
@@ -18,165 +18,165 @@ let roomPlayers = {};
 let deckCards = cards.monuments;
 
 shuffle = (a) => {
-  var j, x, i;
-  for (i = a.length - 1; i > 0; i--) {
-      j = Math.floor(Math.random() * (i + 1));
-      x = a[i];
-      a[i] = a[j];
-      a[j] = x;
-  }
-  return a;
+	var j, x, i;
+	for (i = a.length - 1; i > 0; i--) {
+		j = Math.floor(Math.random() * (i + 1));
+		x = a[i];
+		a[i] = a[j];
+		a[j] = x;
+	}
+	return a;
 }
 
 dispatchCards = (roomId, cardPerPlayer) => {
-  let room = roomPlayers[roomId];
-  let cards = room.cards;
-  // Get all player and played card back to the deck
-  cards = cards.concat(room.playedCards);
+	let room = roomPlayers[roomId];
+	let cards = room.cards;
+	// Get all player and played card back to the deck
+	cards = cards.concat(room.playedCards);
 
-  room.playedCards = [];
-  room.players.forEach(p => {
-    cards = cards.concat(p.cards);
-    p.cards = [];
-  });
+	room.playedCards = [];
+	room.players.forEach(p => {
+		cards = cards.concat(p.cards);
+		p.cards = [];
+	});
 
-  cards = shuffle(cards);
-  room.playedCards.push(cards.shift());
-  for (let i=0; i<cardPerPlayer; i++){
-    // Dispatch the deck to players
-    room.players.forEach(p => {
-      p.cards.push(cards.shift());
-    });
-  }
-  room.cards = cards;
+	cards = shuffle(cards);
+	room.playedCards.push(cards.shift());
+	for (let i = 0; i < cardPerPlayer; i++) {
+		// Dispatch the deck to players
+		room.players.forEach(p => {
+			p.cards.push(cards.shift());
+		});
+	}
+	room.cards = cards;
 }
 
 io.on("connection", (socket) => {
-  
-  // Join a room
-  const { roomId, pseudo } = socket.handshake.query;
-  socket.join(roomId);
 
-  // Init or update the room
-  if (roomPlayers[roomId] === undefined) {
-    // The deck
-    let deck = [...deckCards];
-    // Random starting card
-    let index = Math.floor(Math.random() * deck.length);
-    let playedCards = [...deck.splice(index, 1)];
+	// Join a room
+	const { roomId, pseudo } = socket.handshake.query;
+	socket.join(roomId);
 
-    roomPlayers[roomId] = {
-      players: [{pseudo: pseudo, isMaster: true, id: socket.id, cards: [], isPlaying: false}],
-      playedCards: playedCards,
-      cards: deck
-    };
-  } else {
-    roomPlayers[roomId].players = [...roomPlayers[roomId].players, {
-      pseudo: pseudo, isMaster: false, id: socket.id, cards: [], isPlaying: false
-    }];
-  }
+	// Init or update the room
+	if (roomPlayers[roomId] === undefined) {
+		// The deck
+		let deck = [...deckCards];
+		// Random starting card
+		let index = Math.floor(Math.random() * deck.length);
+		let playedCards = [...deck.splice(index, 1)];
 
-  console.log("connection", roomId, pseudo);
-  // Send the new list of players to all
-  io.in(roomId).emit(PLAYER_EVENT, roomPlayers[roomId].players);
-  // Send room info to the new player
-  io.to(socket.id).emit(PLAYED_CARD_EVENT, roomPlayers[roomId].playedCards);
+		roomPlayers[roomId] = {
+			players: [{ pseudo: pseudo, isMaster: true, id: socket.id, cards: [], isPlaying: false }],
+			playedCards: playedCards,
+			cards: deck
+		};
+	} else {
+		roomPlayers[roomId].players = [...roomPlayers[roomId].players, {
+			pseudo: pseudo, isMaster: false, id: socket.id, cards: [], isPlaying: false
+		}];
+	}
 
-  // Listen for new messages
-  socket.on(NEW_MASSAGE_EVENT, (data) => {
-    console.log("action message", data);
-    if (data.action === 'shuffle'){
-      // Shuffle and give card to every players in the room
-      dispatchCards(roomId, 2);
-      roomPlayers[roomId].players.forEach(p => {
-        //console.log(p.id);
-        p.isPlaying = false;
-        io.to(p.id).emit(PLAYER_CARD_EVENT, p.cards);
-      });
-      // Send the beggining card to everyone
-      io.in(roomId).emit(PLAYED_CARD_EVENT, roomPlayers[roomId].playedCards);
-      // Which player start the game ?
-      let nPlayers = roomPlayers[roomId].players.length;
-      roomPlayers[roomId].players[Math.floor(Math.random() * nPlayers)].isPlaying = true;
-      io.in(roomId).emit(PLAYER_EVENT, roomPlayers[roomId].players);
-      io.in(roomId).emit(WINNER_EVENT, null);
-    } else if (/^play;\d+;\d+/.test(data.action)){
-      // The player is guessing a card
-      let playerIndex = roomPlayers[roomId].players.findIndex(p => p.id === data.senderId);
-      let currentPlayer = roomPlayers[roomId].players[playerIndex];
-      let move = data.action.split(';');
-      move.shift();
-      move = move.map(value => +value);
-      let playerPos = move[0];
-      let boardPos = move[1];
-      // Move the player card and test if it is correct
-      let playerCard = currentPlayer.cards.splice(playerPos, 1)[0];
-      let boardCards = roomPlayers[roomId].playedCards;
-      let goodAnswer = false;
-      if (boardPos === 0){
-        //console.log(`${playerCard.intSolution} <= ${boardCards[boardPos].intSolution}`);
-        goodAnswer = (playerCard.intSolution <= boardCards[boardPos].intSolution);
-      } else if (boardPos === boardCards.length){
-        //console.log(`${playerCard.intSolution} >= ${boardCards[boardPos-1].intSolution}`);
-        goodAnswer = (playerCard.intSolution >= boardCards[boardPos-1].intSolution);
-      } else {
-        //console.log(`${boardCards[boardPos-1].intSolution} <= ${playerCard.intSolution} <= ${boardCards[boardPos].intSolution}`);
-        goodAnswer = (boardCards[boardPos-1].intSolution <= playerCard.intSolution &&
-                      playerCard.intSolution <= boardCards[boardPos].intSolution);
-      }
-      if (goodAnswer){
-        // Put the card in the played card
-        boardCards.splice(boardPos, 0, playerCard);
-      } else {
-        // Put the card back in the deck and give another card to the current player
-        roomPlayers[roomId].cards.push(playerCard);
-        currentPlayer.cards.push(roomPlayers[roomId].cards.shift());
-      }
-      let message = `${goodAnswer ? '✅' : '❌'} ${currentPlayer.pseudo}, ${playerCard.name}${goodAnswer ? '' : ` (${playerCard.solution})`}`;
-      io.in(roomId).emit(NEW_MASSAGE_EVENT, message);
-      if (goodAnswer && currentPlayer.cards.length === 0){
-        // we have a winner
-        io.in(roomId).emit(WINNER_EVENT, currentPlayer);
-        io.in(roomId).emit(PLAYED_CARD_EVENT, roomPlayers[roomId].playedCards);
-      } else {
-        // give the turn to the next player and send the turn update
-        currentPlayer.isPlaying = false;
-        if (playerIndex+1 === roomPlayers[roomId].players.length){
-          playerIndex = -1;
-        }
-        roomPlayers[roomId].players[playerIndex+1].isPlaying = true;
-        io.in(roomId).emit(PLAYER_EVENT, roomPlayers[roomId].players);
-        if (goodAnswer){
-          io.in(roomId).emit(PLAYED_CARD_EVENT, roomPlayers[roomId].playedCards);
-        }
-        io.to(data.senderId).emit(PLAYER_CARD_EVENT, currentPlayer.cards);
-      }
-    }
-  });
+	console.log("connection", roomId, pseudo);
+	// Send the new list of players to all
+	io.in(roomId).emit(PLAYER_EVENT, roomPlayers[roomId].players);
+	// Send room info to the new player
+	io.to(socket.id).emit(PLAYED_CARD_EVENT, roomPlayers[roomId].playedCards);
 
-  // Leave the room if the user closes the socket
-  socket.on("disconnect", () => {
-    console.log("disconnect", roomId, pseudo);
-    // Find the leaving player
-    let leavingPlayer = roomPlayers[roomId].players.find(p => p.id === socket.id);
-    // Remove it
-    roomPlayers[roomId].players = roomPlayers[roomId].players.filter(p => p.id !== socket.id);
-    // Put back the player card in the deck
-    roomPlayers[roomId].cards = [...roomPlayers[roomId].cards, ...leavingPlayer.cards];
-    // If the room is empty, delete
-    if (roomPlayers[roomId].players.length === 0){
-      delete roomPlayers[roomId];
-    } else {
-      // If the leaving player is the master, change it
-      if (leavingPlayer.isMaster){
-        roomPlayers[roomId].players[0]["isMaster"] = true;
-      }
-      io.in(roomId).emit(PLAYER_EVENT, roomPlayers[roomId].players);
-    }
-    socket.leave(roomId);
-  });
+	// Listen for new messages
+	socket.on(NEW_MASSAGE_EVENT, (data) => {
+		console.log("action message", data);
+		if (data.action === 'shuffle') {
+			// Shuffle and give card to every players in the room
+			dispatchCards(roomId, 2);
+			roomPlayers[roomId].players.forEach(p => {
+				//console.log(p.id);
+				p.isPlaying = false;
+				io.to(p.id).emit(PLAYER_CARD_EVENT, p.cards);
+			});
+			// Send the beggining card to everyone
+			io.in(roomId).emit(PLAYED_CARD_EVENT, roomPlayers[roomId].playedCards);
+			// Which player start the game ?
+			let nPlayers = roomPlayers[roomId].players.length;
+			roomPlayers[roomId].players[Math.floor(Math.random() * nPlayers)].isPlaying = true;
+			io.in(roomId).emit(PLAYER_EVENT, roomPlayers[roomId].players);
+			io.in(roomId).emit(WINNER_EVENT, null);
+		} else if (/^play;\d+;\d+/.test(data.action)) {
+			// The player is guessing a card
+			let playerIndex = roomPlayers[roomId].players.findIndex(p => p.id === data.senderId);
+			let currentPlayer = roomPlayers[roomId].players[playerIndex];
+			let move = data.action.split(';');
+			move.shift();
+			move = move.map(value => +value);
+			let playerPos = move[0];
+			let boardPos = move[1];
+			// Move the player card and test if it is correct
+			let playerCard = currentPlayer.cards.splice(playerPos, 1)[0];
+			let boardCards = roomPlayers[roomId].playedCards;
+			let goodAnswer = false;
+			if (boardPos === 0) {
+				//console.log(`${playerCard.intSolution} <= ${boardCards[boardPos].intSolution}`);
+				goodAnswer = (playerCard.intSolution <= boardCards[boardPos].intSolution);
+			} else if (boardPos === boardCards.length) {
+				//console.log(`${playerCard.intSolution} >= ${boardCards[boardPos-1].intSolution}`);
+				goodAnswer = (playerCard.intSolution >= boardCards[boardPos - 1].intSolution);
+			} else {
+				//console.log(`${boardCards[boardPos-1].intSolution} <= ${playerCard.intSolution} <= ${boardCards[boardPos].intSolution}`);
+				goodAnswer = (boardCards[boardPos - 1].intSolution <= playerCard.intSolution &&
+					playerCard.intSolution <= boardCards[boardPos].intSolution);
+			}
+			if (goodAnswer) {
+				// Put the card in the played card
+				boardCards.splice(boardPos, 0, playerCard);
+			} else {
+				// Put the card back in the deck and give another card to the current player
+				roomPlayers[roomId].cards.push(playerCard);
+				currentPlayer.cards.push(roomPlayers[roomId].cards.shift());
+			}
+			let message = `${goodAnswer ? '✅' : '❌'} ${currentPlayer.pseudo}, ${playerCard.name}${goodAnswer ? '' : ` (${playerCard.solution})`}`;
+			io.in(roomId).emit(NEW_MASSAGE_EVENT, message);
+			if (goodAnswer && currentPlayer.cards.length === 0) {
+				// we have a winner
+				io.in(roomId).emit(WINNER_EVENT, currentPlayer);
+				io.in(roomId).emit(PLAYED_CARD_EVENT, roomPlayers[roomId].playedCards);
+			} else {
+				// give the turn to the next player and send the turn update
+				currentPlayer.isPlaying = false;
+				if (playerIndex + 1 === roomPlayers[roomId].players.length) {
+					playerIndex = -1;
+				}
+				roomPlayers[roomId].players[playerIndex + 1].isPlaying = true;
+				io.in(roomId).emit(PLAYER_EVENT, roomPlayers[roomId].players);
+				if (goodAnswer) {
+					io.in(roomId).emit(PLAYED_CARD_EVENT, roomPlayers[roomId].playedCards);
+				}
+				io.to(data.senderId).emit(PLAYER_CARD_EVENT, currentPlayer.cards);
+			}
+		}
+	});
+
+	// Leave the room if the user closes the socket
+	socket.on("disconnect", () => {
+		console.log("disconnect", roomId, pseudo);
+		// Find the leaving player
+		let leavingPlayer = roomPlayers[roomId].players.find(p => p.id === socket.id);
+		// Remove it
+		roomPlayers[roomId].players = roomPlayers[roomId].players.filter(p => p.id !== socket.id);
+		// Put back the player card in the deck
+		roomPlayers[roomId].cards = [...roomPlayers[roomId].cards, ...leavingPlayer.cards];
+		// If the room is empty, delete
+		if (roomPlayers[roomId].players.length === 0) {
+			delete roomPlayers[roomId];
+		} else {
+			// If the leaving player is the master, change it
+			if (leavingPlayer.isMaster) {
+				roomPlayers[roomId].players[0]["isMaster"] = true;
+			}
+			io.in(roomId).emit(PLAYER_EVENT, roomPlayers[roomId].players);
+		}
+		socket.leave(roomId);
+	});
 });
 
 server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+	console.log(`Listening on port ${PORT}`);
 });
